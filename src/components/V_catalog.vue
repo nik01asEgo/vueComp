1b68b8c654d5f92e573665a3496db43568c579ae
nik01asEgo <nikolas85@inbox.ru>
2022-07-21 16:38:38 +0300

    Create_Catalog

--- src/components/V_catalog.vue
+++ src/components/V_catalog.vue
@@ -1,21 +1,96 @@
 <template>
-  <div>
-    <p>{{ title }}</p>
+  <div class="v_catalog">
+   
+    <vCatalogItem 
+      v-for= "item in cats"
+      :key= "item.article"
+      :item_data= "item"
+      >
+    </vCatalogItem>
   </div>
 </template>
 
 <script>
-export default {
-  name: "v_catalog",
-  created() {},
-  data() {
-    return {
-      title: 'Yes i am a catalog'
-    };
-  },
+  import vCatalogItem from './V_catalog_item.vue';
+  export default {
+    name: "v_catalog",
+    components: {
+      vCatalogItem
+    },
+    created() {},
+    data() {
+      return {
+       cats: [
+          {
+          image: "1.webp",
+          name: "Мейн кун",
+          city: "Москва",
+          price: "80 Р",
+          owner: "Вероника",
+          article: "c1",
+          available: true
+          },
+          {
+          image: "2.webp",
+          name: "Продажа",
+          city: "Москва",
+          price: "40000 Р",
+          owner: "Татьяна",
+          article: "c2",
+          available: true
+          },
+          {
+          image: "3.webp",
+          name: "Продажа",
+          city: "Москва",
+          price: "40000 Р",
+          owner: "Татьяна",
+          article: "c3",
+          available: true
+          },
+          {
+          image: "4.webp",
+          name: "3,5 мес принцесса Мура ищет семью!",
+          city: "Москва",
+          price: "10 Р",
+          owner: "Светлана",
+          article: "c4",
+          available: true
+          },
+          {
+          image: "5.webp",
+          name: "Симон и Сима. Котята в добрые руки",
+          city: "Москва",
+          price: "Бесплатно",
+          owner: "Светлана",
+          article: "c5",
+          available: true
+          },
+          {
+          image: "6.webp",
+          name: "Маленькие котята в добрые руки",
+          city: "Москва",
+          price: "10 Р",
+          owner: "Светлана",
+          article: "c6",
+          available: true
+          } 
+       ]
+      };
+    },
   props: {},
   methods: {},
-};
+  };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+  .v_catalog {
+    max-width: 1200 px;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: stretch;
+    align-items: center;
+    background-color: #f4f7f9;
+    flex: 0 1 33,3%;
+  }
+</style>
---
+++ src/components/V_catalog_item.vue.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class= "v_catalog_item">
+    <a href="http://google.com">
+      <img class="v_catalog_item_image" 
+      :src="require ('../assets/images/' + item_data.image)" 
+      alt= "webp">
+    </a>        
+    <a class="v_catalog_item_name" href="http://google.com">
+        <p >{{ item_data.name }}</p>
+    </a>
+    
+    <a class="v_catalog_item_city" href="http://google.com">
+        <p >{{ item_data.city }}</p>
+    </a>
+
+        <p class="v_catalog_item_price">{{ item_data.price }}</p>
+
+    <a class="v_catalog_item_owner" href="http://google.com">
+        <p >{{ item_data.owner }}</p>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "V_catalog_item",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {
+    item_data: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  methods: {},
+};
+</script>
+  
+<style scoped>
+  .v_catalog_item {
+    display: block;
+    border: 1px solid;
+    position:relative;
+    text-align: center;
+    max-width: 33%;
+    height: 500 px;
+    background-color: rgb(255, 255, 255);
+  }
+  .v_catalog_item_image {
+    
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  margin: 20px;
+  object-fit: cover;
+  display: block;
+  margin: auto;
+    
+  }
+.v_catalog_item_name {
+
+  text-decoration: none;
+  color: rgb(12, 12, 12);
+  font-family: Montserrat,sans-serif;
+  font-size: 19px;
+  font-weight: 500;
+  text-align: left;
+  margin-left: 15 px;
+
+}
+.v_catalog_item_city {
+  
+  text-decoration: none;
+  color: #777;
+  font-family: Montserrat,sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  text-align: left;
+  
+}
+.v_catalog_item_owner {
+
+  text-decoration: none;
+  color: #777;
+  font-family: Montserrat,sans-serif;
+  font-size: 15px;
+  font-weight: 500;
+  text-align: left;
+
+}
+.v_catalog_item_price {
+
+  text-decoration: none;
+  color: rgb(12, 12, 12);
+  font-family: Montserrat,sans-serif;
+  font-size: 19px;
+  font-weight: 500;
+  text-align: left;
+}
+
+</style>
